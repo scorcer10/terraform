0dd09e57b52b0f4d22cd43b8e6219c527182803d
scorcer10 <109479715+scorcer10@users.noreply.github.com>
2025-06-16 20:46:56 +0530

    version fix

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
-provider "aws" {
-    region = "us-west-2"  
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
+provider "aws" {
+  region = "us-west-2"
+}
 resource "aws_instance" "foo" {
   ami           = "ami-0779fe5e56472b841"  # Amazon Linux 2023 (ARM)
   instance_type = "t4g.micro"
