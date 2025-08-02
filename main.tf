3f6a1a260cd49277f9a9f1ef715689a174cb7370
Manisha Sharma <manisha.sharma@autodesk.com>
2025-08-02 17:03:11 +0530

    Fix AMI permissions issue - use hardcoded AMI ID

--- main.tf
+++ main.tf
@@ -12,29 +12,8 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-# Data source to get the latest Amazon Linux 2023 ARM64 AMI
-data "aws_ami" "amazon_linux_2023_arm64" {
-  most_recent = true
-  owners      = ["amazon"]
-  
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*"]
-  }
-  
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-  
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-}
-
 resource "aws_instance" "foo" {
-  ami           = data.aws_ami.amazon_linux_2023_arm64.id
+  ami           = "ami-0387d929287ab193e"  # Amazon Linux 2023 (ARM64) - us-west-2 
   instance_type = "t4g.micro"
   tags = {
       Name = "TF-Instance"
