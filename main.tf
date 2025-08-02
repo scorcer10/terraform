66517e038f9398bb7e84293286c8545ced022b0e
Manisha Sharma <manisha.sharma@autodesk.com>
2025-08-02 18:09:50 +0530

    Replace EC2 instance with S3 bucket for testing IAM permissions

--- main.tf
+++ main.tf
@@ -12,10 +12,23 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-resource "aws_instance" "foo" {
-  ami           = "ami-0387d929287ab193e"  # Amazon Linux 2023 (x86_64) - us-west-2 
-  instance_type = "t3.micro"
+# Simple S3 bucket for testing (requires fewer permissions)
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "terraform-test-bucket-${random_id.bucket_suffix.hex}"
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Test-Bucket"
   }
 }
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# Uncomment this when you have EC2 permissions
+# resource "aws_instance" "foo" {
+#   ami           = "ami-0387d929287ab193e"  # Amazon Linux 2023 (x86_64) - us-west-2 
+#   instance_type = "t3.micro"
+#   tags = {
+#       Name = "TF-Instance"
+#   }
+# }
