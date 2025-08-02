2bb51bab702654ba173ce444809bb725d666d7b5
Manisha Sharma <manisha.sharma@autodesk.com>
2025-08-02 17:06:15 +0530

    Fix architecture mismatch - use t3.micro for x86_64 AMI

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ provider "aws" {
   region = "us-west-2"
 }
 resource "aws_instance" "foo" {
-  ami           = "ami-0387d929287ab193e"  # Amazon Linux 2023 (ARM64) - us-west-2 
-  instance_type = "t4g.micro"
+  ami           = "ami-0387d929287ab193e"  # Amazon Linux 2023 (x86_64) - us-west-2 
+  instance_type = "t3.micro"
   tags = {
       Name = "TF-Instance"
   }
