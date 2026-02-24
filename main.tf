76e1e3481d36d751e9c2abeb811129d37800090a
Manisha Sharma <manisha.sharma@autodesk.com>
2026-02-24 23:33:43 +0530

    ec2 instance

--- main.tf
+++ main.tf
@@ -12,11 +12,11 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-# Simple S3 bucket for testing (requires fewer permissions)
+# Simple S3 bucket for testing
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "terraform-test-bucket-${random_id.bucket_suffix.hex}"
   tags = {
-    Name = "TF-Test-Bucket"
+    Name = "TF-Test"
   }
 }
 
@@ -24,11 +24,11 @@ resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-# Uncomment this when you have EC2 permissions
-# resource "aws_instance" "foo" {
-#   ami           = "ami-0387d929287ab193e"  # Amazon Linux 2023 (x86_64) - us-west-2 
-#   instance_type = "t3.micro"
-#   tags = {
-#       Name = "TF-Instance"
-#   }
-# }
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0387d929287ab193e"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name        = "TF-Test"
+  }
+}
